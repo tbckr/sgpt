5bfa9c6b771b028bb1f2e648868c6921c01bca25
Tim <11543666+tbckr@users.noreply.github.com>
2023-03-05 16:53:52 +0100

    feat: add support for chat completions

--- sgpt.go
+++ sgpt.go
@@ -6,7 +6,7 @@ import (
 	"errors"
 	"flag"
 	"fmt"
-	gogpt "github.com/sashabaranov/go-openai"
+	openai "github.com/sashabaranov/go-openai"
 	"os"
 	"os/exec"
 	"runtime"
@@ -21,7 +21,7 @@ var (
 )
 
 var (
-	model       = flag.String("model", "text-davinci-003", "GPT-3 model name")
+	model       = flag.String("model", "gpt-3.5-turbo", "GPT-3 model name")
 	maxTokens   = flag.Int("max-tokens", 2048, "Strict length of output (words)")
 	temperature = flag.Float64("temperature", float64(1), "Randomness of generated output")
 	topP        = flag.Float64("top-p", float64(1), "Limits highest probable tokens")
@@ -29,6 +29,17 @@ var (
 	execute     bool
 	code        = flag.Bool("code", false, "Provide code as output")
 )
+
+type configStruct struct {
+	prompt      string
+	model       string
+	maxTokens   int
+	temperature float32
+	topP        float32
+	shell       bool
+	execute     bool
+	code        bool
+}
 
 func init() {
 	if runtime.GOOS == "windows" {
@@ -49,34 +60,15 @@ func init() {
 	flag.BoolVar(&execute, "e", executeDefault, executeUsage)
 }
 
-func Run() error {
-	flag.Parse()
-
-	// TODO: retrieve prompt via stdin
-
-	var prompt = ""
-
-	// Check, if prompt was provided via command line
-	if prompt == "" && flag.NArg() != 1 {
-		return errors.New("A prompt must be provided")
-	}
-	prompt = flag.Arg(0)
-
-	if shell {
-		// If default values where not changed, make it more accurate
-		if *temperature == float64(1) && *temperature == *topP {
+func createConfig(prompt string) configStruct {
+	// If default values where not changed, make it more accurate
+	if *temperature == float64(1) && *temperature == *topP {
+		if shell {
 			*temperature = 0.2
 			*topP = 0.9
-			// add directive for shell to prompt
-			prompt += ". Provide only shell command as output."
-		}
-	} else if *code {
-		// If default values where not changed, make it more accurate
-		if *temperature == float64(1) && *temperature == *topP {
+		} else if *code {
 			*temperature = 0.8
 			*topP = 0.2
-			// add directive for code to prompt
-			prompt += ". Provide only code as output."
 		}
 	}
 
@@ -85,31 +77,113 @@ func Run() error {
 		*maxTokens = 1024
 	}
 
-	// Check if api key was set
+	return configStruct{
+		prompt:      prompt,
+		model:       *model,
+		maxTokens:   *maxTokens,
+		temperature: float32(*temperature),
+		topP:        float32(*topP),
+		shell:       shell,
+		execute:     execute,
+		code:        *code,
+	}
+}
+
+func getCompletion(ctx context.Context, client *openai.Client, config configStruct) (string, error) {
+	var prompt string
+	if config.shell {
+		// add directive for shell to prompt
+		prompt = config.prompt + ". Provide only shell command as output."
+	} else if config.code {
+		// add directive for code to prompt
+		prompt = config.prompt + ". Provide only code as output."
+	}
+
+	req := openai.CompletionRequest{
+		Model:       config.model,
+		MaxTokens:   config.maxTokens,
+		Prompt:      prompt,
+		Temperature: config.temperature,
+		TopP:        config.topP,
+	}
+
+	resp, err := client.CreateCompletion(ctx, req)
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(resp.Choices[0].Text), nil
+}
+
+func getChatCompletion(ctx context.Context, client *openai.Client, config configStruct) (string, error) {
+	var messages []openai.ChatCompletionMessage
+	if config.shell {
+		messages = append(messages, openai.ChatCompletionMessage{
+			Role:    openai.ChatMessageRoleSystem,
+			Content: "Provide only shell command as output.",
+		})
+	} else if config.code {
+		messages = append(messages, openai.ChatCompletionMessage{
+			Role:    openai.ChatMessageRoleSystem,
+			Content: "Provide only code as output.",
+		})
+	}
+	messages = append(messages, openai.ChatCompletionMessage{
+		Role:    openai.ChatMessageRoleUser,
+		Content: config.prompt,
+	})
+
+	req := openai.ChatCompletionRequest{
+		Model:       config.model,
+		MaxTokens:   config.maxTokens,
+		Messages:    messages,
+		Temperature: config.temperature,
+		TopP:        config.topP,
+	}
+
+	resp, err := client.CreateChatCompletion(ctx, req)
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(resp.Choices[0].Message.Content), nil
+}
+
+func Run() error {
+	flag.Parse()
+
+	// Check, if prompt was provided via command line
+	if flag.NArg() != 1 {
+		return errors.New("A prompt must be provided")
+	}
+
+	// Check, if api key was set
 	apiKey, exists := os.LookupEnv(EnvKey)
 	if !exists {
 		return fmt.Errorf("%s is not set", EnvKey)
 	}
 
+	// Create config with flags
+	config := createConfig(flag.Arg(0))
+
 	// Create api client and do request
-	c := gogpt.NewClient(apiKey)
+	client := openai.NewClient(apiKey)
 	ctx := context.Background()
 
-	req := gogpt.CompletionRequest{
-		Model:       *model,
-		MaxTokens:   *maxTokens,
-		Prompt:      prompt,
-		Temperature: float32(*temperature),
+	var response string
+	var err error
+	if config.model == openai.GPT3Dot5Turbo || config.model == openai.GPT3Dot5Turbo0301 {
+		response, err = getChatCompletion(ctx, client, config)
+	} else {
+		response, err = getCompletion(ctx, client, config)
 	}
-	resp, err := c.CreateCompletion(ctx, req)
 	if err != nil {
 		return err
 	}
-	responseText := strings.TrimSpace(resp.Choices[0].Text)
 
 	// Execute response in shell, if flags are provided
-	if shell && execute {
-		fmt.Println(ColorRed + responseText + ColorReset)
+	if config.shell && config.execute {
+		fmt.Println(ColorRed + response + ColorReset)
 
 		// Require user confirmation
 		for {
@@ -128,13 +202,14 @@ func Run() error {
 		}
 
 		// Execute cmd from response text
-		cmd := exec.CommandContext(ctx, "bash", "-c", responseText)
+		cmd := exec.CommandContext(ctx, "bash", "-c", response)
 		cmd.Stdout = os.Stdout
 		if err := cmd.Run(); err != nil {
 			return err
 		}
+
 	} else {
-		fmt.Println(responseText)
+		fmt.Println(response)
 	}
 
 	return nil
