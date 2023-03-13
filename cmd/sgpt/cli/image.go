c3a546f4e6f19a52ff8b46db421c6c2faea38ce3
Tim <11543666+tbckr@users.noreply.github.com>
2023-03-13 18:01:37 +0100

    feat: add dalle support

--- cmd/sgpt/cli/image.go
+++ cmd/sgpt/cli/image.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"strings"
@@ -19,20 +20,20 @@ var imageCmd = &ffcli.Command{
 	FlagSet: (func() *flag.FlagSet {
 		fs := newFlagSet("image")
 		fs.IntVar(&imageArgs.count, "n", 1, "Number of images to generate")
-		fs.BoolVar(&imageArgs.open, "open", false, "Open image in default browser")
-		fs.BoolVar(&imageArgs.download, "download", false, "Download generated images")
-		fs.StringVar(&imageArgs.directory, "output", ".", "Path to folder to save the image in")
+		//fs.BoolVar(&imageArgs.open, "open", false, "Open image in default browser")
+		//fs.BoolVar(&imageArgs.download, "download", false, "Download generated images")
+		//fs.StringVar(&imageArgs.outputDirectory, "output", ".", "Path to folder to save the image in")
 		fs.BoolVar(&imageArgs.json, "json", false, "Output image urls in json format")
 		return fs
 	})(),
 }
 
 var imageArgs struct {
-	count     int
-	open      bool
-	download  bool
-	directory string
-	json      bool
+	count           int
+	open            bool
+	download        bool
+	outputDirectory string
+	json            bool
 }
 
 func runImage(ctx context.Context, args []string) error {
@@ -56,10 +57,58 @@ func runImage(ctx context.Context, args []string) error {
 	if err != nil {
 		return err
 	}
-	for _, element := range imageUrls {
-		if _, err = fmt.Fprint(stdout, element); err != nil {
+	return handleImageURLs(imageUrls)
+}
+
+func handleImageURLs(imageUrls []string) error {
+	if imageArgs.json {
+		if err := printURLsAsJSON(imageUrls); err != nil {
+			return err
+		}
+	} else {
+		if err := printURLs(imageUrls); err != nil {
+			return err
+		}
+	}
+
+	//if imageArgs.download {
+	//	if err := downloadImages(imageUrls); err != nil {
+	//		return err
+	//	}
+	//}
+	//if imageArgs.open {
+	//	if err := openImages(imageUrls); err != nil {
+	//		return err
+	//	}
+	//}
+	return nil
+}
+
+func printURLs(urls []string) error {
+	for _, element := range urls {
+		if _, err := fmt.Fprint(stdout, element); err != nil {
 			return err
 		}
 	}
 	return nil
 }
+
+func printURLsAsJSON(urls []string) error {
+	urlMap := map[string][]string{
+		"imageURLs": urls,
+	}
+	jsonData, err := json.Marshal(urlMap)
+	if err != nil {
+		return err
+	}
+	_, err = fmt.Fprintln(stdout, jsonData)
+	return err
+}
+
+//func openImages(_ []string) error {
+//	return nil
+//}
+//
+//func downloadImages(_ []string) error {
+//	return nil
+//}
