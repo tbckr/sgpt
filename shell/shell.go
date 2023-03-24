c39db3239dfe9362fc75886638ea219b731aa507
Tim <11543666+tbckr@users.noreply.github.com>
2023-03-24 19:14:02 +0100

    feat: add logging to shell

--- shell/shell.go
+++ shell/shell.go
@@ -28,6 +28,8 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+
+	jww "github.com/spf13/jwalterweatherman"
 )
 
 var ErrMissingInput = errors.New("no input was provided")
@@ -44,12 +46,15 @@ func GetInput(args []string) (string, error) {
 		if err != nil {
 			return "", err
 		}
+		jww.DEBUG.Println("Input via stdin was provided")
 	} else {
 		// Check, if prompt was provided via command line
 		if len(args) != 1 {
+			jww.ERROR.Println("No input via args was provided")
 			return "", ErrMissingInput
 		}
 		prompt = args[0]
+		jww.DEBUG.Println("Input via args was provided")
 	}
 	return prompt, nil
 }
@@ -57,11 +62,14 @@ func GetInput(args []string) (string, error) {
 func IsPipedShell() (bool, error) {
 	fi, err := os.Stdin.Stat()
 	if err != nil {
+		jww.ERROR.Println("Could not get stdin info")
 		return false, err
 	}
 	if fi.Mode()&os.ModeNamedPipe == 0 {
+		jww.DEBUG.Println("Input via stdin was not provided")
 		return false, nil
 	}
+	jww.DEBUG.Println("Input via stdin was provided")
 	return true, nil
 }
 
@@ -72,14 +80,18 @@ func GetPipedData() (string, error) {
 		buf = append(buf, scanner.Bytes()...)
 	}
 	if err := scanner.Err(); err != nil {
+		jww.ERROR.Println("Could not read data from stdin")
 		return "", err
 	}
-	return string(buf), nil
+	input := string(buf)
+	jww.DEBUG.Printf("Input via stdin was: %s", input)
+	return input, nil
 }
 
 func ExecuteCommandWithConfirmation(ctx context.Context, bashCommand string) error {
 	// Print out the command to be executed
 	if _, err := fmt.Fprintln(os.Stdout, bashCommand); err != nil {
+		jww.ERROR.Println("Could not print command to be executed")
 		return err
 	}
 	// Require user confirmation
@@ -95,27 +107,37 @@ func ExecuteCommandWithConfirmation(ctx context.Context, bashCommand string) err
 
 func GetUserConfirmation() (bool, error) {
 	for {
+		jww.DEBUG.Println("Waiting for user confirmation")
 		if _, err := fmt.Fprint(os.Stdout, "Do you want to execute this command? (Y/n) "); err != nil {
 			return false, err
 		}
 		reader := bufio.NewReader(os.Stdin)
 		char, _, err := reader.ReadRune()
 		if err != nil {
+			jww.ERROR.Println("Could not read user input for confirmation")
 			return false, err
 		}
 		// 10 = enter
 		if char == 10 || char == 'Y' || char == 'y' {
+			jww.DEBUG.Println("User confirmed execution")
 			return true, nil
 		} else if char == 'N' || char == 'n' {
+			jww.DEBUG.Println("User denied execution")
 			return false, nil
 		}
 	}
 }
 
 func ExecuteShellCommand(ctx context.Context, response string) error {
+	jww.DEBUG.Println("Executing command: ", response)
 	// Execute cmd from response text
 	cmd := exec.CommandContext(ctx, "bash", "-c", response)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	return cmd.Run()
+	err := cmd.Run()
+	if err != nil {
+		jww.ERROR.Println("Error while executing command: ", err)
+		return err
+	}
+	return nil
 }
