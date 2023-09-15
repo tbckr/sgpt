a2ec2788218d7f49dacfbd6db197d6d44d513320
Tim <11543666+tbckr@users.noreply.github.com>
2023-09-15 22:58:04 +0200

    chore: add completion and man test

---
+++ cli/completion_test.go
@@ -0,0 +1,122 @@
+// Copyright (c) 2023 Tim <tbckr>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// SPDX-License-Identifier: MIT
+
+package cli
+
+import (
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCompletionCmd(t *testing.T) {
+	mem := &exitMemento{}
+
+	config := createTestConfig(t)
+	defer teardownTestDirs(t, config)
+
+	root := newRootCmd(mem.Exit, config, nil)
+	root.cmd.SetOut(io.Discard)
+	root.Execute([]string{"completion"})
+
+	require.Equal(t, 0, mem.code)
+}
+
+func TestCompletionCmdBash(t *testing.T) {
+	mem := &exitMemento{}
+
+	config := createTestConfig(t)
+	defer teardownTestDirs(t, config)
+
+	root := newRootCmd(mem.Exit, config, nil)
+	root.cmd.SetOut(io.Discard)
+	root.Execute([]string{"completion", "bash"})
+
+	require.Equal(t, 0, mem.code)
+}
+
+func TestCompletionCmdFish(t *testing.T) {
+	mem := &exitMemento{}
+
+	config := createTestConfig(t)
+	defer teardownTestDirs(t, config)
+
+	root := newRootCmd(mem.Exit, config, nil)
+	root.cmd.SetOut(io.Discard)
+	root.Execute([]string{"completion", "fish"})
+
+	require.Equal(t, 0, mem.code)
+}
+
+func TestCompletionCmdPowershell(t *testing.T) {
+	mem := &exitMemento{}
+
+	config := createTestConfig(t)
+	defer teardownTestDirs(t, config)
+
+	root := newRootCmd(mem.Exit, config, nil)
+	root.cmd.SetOut(io.Discard)
+	root.Execute([]string{"completion", "powershell"})
+
+	require.Equal(t, 0, mem.code)
+}
+
+func TestCompletionCmdZsh(t *testing.T) {
+	mem := &exitMemento{}
+
+	config := createTestConfig(t)
+	defer teardownTestDirs(t, config)
+
+	root := newRootCmd(mem.Exit, config, nil)
+	root.cmd.SetOut(io.Discard)
+	root.Execute([]string{"completion", "zsh"})
+
+	require.Equal(t, 0, mem.code)
+}
+
+func TestCompletionCmdUnknownCompletion(t *testing.T) {
+	mem := &exitMemento{}
+
+	config := createTestConfig(t)
+	defer teardownTestDirs(t, config)
+
+	root := newRootCmd(mem.Exit, config, nil)
+	root.cmd.SetOut(io.Discard)
+	root.Execute([]string{"completion", "abcd"})
+
+	// TODO this is inconsistent, should be 1
+	require.Equal(t, 0, mem.code)
+}
+
+func TestCompletionCmdTooManyArgs(t *testing.T) {
+	mem := &exitMemento{}
+
+	config := createTestConfig(t)
+	defer teardownTestDirs(t, config)
+
+	root := newRootCmd(mem.Exit, config, nil)
+	root.cmd.SetOut(io.Discard)
+	root.Execute([]string{"completion", "abcd", "efgh"})
+
+	// TODO this is inconsistent, should be 1
+	require.Equal(t, 0, mem.code)
+}
---
+++ cli/util_test.go
@@ -0,0 +1,75 @@
+// Copyright (c) 2023 Tim <tbckr>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// SPDX-License-Identifier: MIT
+
+package cli
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/require"
+)
+
+type exitMemento struct {
+	code int
+}
+
+func (e *exitMemento) Exit(i int) {
+	e.code = i
+}
+
+func createTestConfig(t *testing.T) *viper.Viper {
+	cacheDir, err := createTempDir("cache")
+	require.NoError(t, err)
+
+	var configDir string
+	configDir, err = createTempDir("config")
+	require.NoError(t, err)
+
+	config := viper.New()
+	config.AddConfigPath(configDir)
+	config.SetConfigFile("config")
+	config.SetConfigType("yaml")
+	config.Set("cacheDir", cacheDir)
+
+	return config
+}
+
+func teardownTestDirs(t *testing.T, config *viper.Viper) {
+	cacheDir := config.GetString("cacheDir")
+	require.NoError(t, removeTempDir(cacheDir))
+
+	configDir := config.ConfigFileUsed()
+	require.NoError(t, removeTempDir(configDir))
+}
+
+func createTempDir(suffix string) (string, error) {
+	if suffix != "" {
+		suffix = "_" + suffix
+	}
+	return os.MkdirTemp("", strings.Join([]string{"sgpt_temp_*", suffix}, ""))
+}
+
+func removeTempDir(fullConfigPath string) error {
+	return os.RemoveAll(fullConfigPath)
+}
