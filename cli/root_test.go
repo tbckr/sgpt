c69fc459cc893f9fda6982743b3e45c3a5339e87
Tim <11543666+tbckr@users.noreply.github.com>
2023-09-30 17:56:06 +0200

    chore(debug): verbose output for clipboard test

--- cli/root_test.go
+++ cli/root_test.go
@@ -114,7 +114,7 @@ func TestRootCmd_SimpleClipboard(t *testing.T) {
 
 	root := newRootCmd(mem.Exit, config, mockIsPipedShell(false, nil), api.MockClient(strings.Clone(expected), nil))
 
-	root.Execute([]string{"--clipboard", prompt})
+	root.Execute([]string{"--clipboard", "-v", prompt})
 	require.Equal(t, 0, mem.code)
 	textInClipboard, _ := clipboard.ReadAll()
 	require.Equal(t, expected, textInClipboard)
